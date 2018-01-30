33fddc095198b3b55cddb13899dc7a3b94bcbf52
mazarag2 <mazarag2@asu.edu>
2018-01-29 20:51:06 -0700

    without server or onload

--- test/testrunner.js
+++ test/testrunner.js
@@ -9,10 +9,12 @@ var port = "8080";
 
 
 // We need the Service 
+/*
 var service = server.listen(8081, {keepAlive:true}, function() {
 	
 });
-if (service) {
+
+//if (service) {
 	//var page = new WebPage();
 	var url = "http://localhost:" + "8080/";
 	console.log("SENDING REQUEST TO:");
@@ -39,19 +41,38 @@ if (service) {
 	   page.render('home.png');
 	   phantom.exit();
 	});
-}
-else{
+//}
+//else{
 	
 	console.log("Unable to create server");
 	phantom.exit(1);
-}
+//}
+*/
 page.open("http://localhost:8080/" ,function(status) {
 	console.log(status);
+	var ua = page.evaluate(function() {
+	  console.log("DOC" + document);
+	  //console.log(document.querySelectorAll('.pageheader')[0]);
+	  return document.getElementsByTagName('h1')[0].innerHTML;
+	});
+	
+	if(!ua){
+		
+		console.log('Home page not found');
+		phantom.exit(1);
+	}
+	else{
+		console.log("Success retreiving Title " + ua);
+		phantom.exit()
+	}
+	/*
 	page.onResourceError = function(resourceError) {
 		 console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
 		  console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
 	};
 	page.onLoadFinished = function(status) {
+		
+		console.log(status);
 		system.stderr.writeLine('= onLoadFinished()');
 		system.stderr.writeLine('  status: ' + status);
 		if (status !== 'success') {
@@ -77,5 +98,6 @@ page.open("http://localhost:8080/" ,function(status) {
 		
 	
 	};
+	*/
 });
 
