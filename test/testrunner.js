4e4658f15e1d864a327e63fe4c9e279f24575c54
mazarag2 <mazarag2@asu.edu>
2018-01-29 21:20:26 -0700

    Aded Debugging for Travis CI

--- test/testrunner.js
+++ test/testrunner.js
@@ -51,12 +51,58 @@ var service = server.listen(8081, {keepAlive:true}, function() {
 var url = 'http://localhost:8080/';
 page.open(url ,function(status) {
 	console.log(status);
-	/*
+	page.onResourceRequested = function (request) {
+		system.stderr.writeLine('= onResourceRequested()');
+		system.stderr.writeLine('  request: ' + JSON.stringify(request, undefined, 4));
+	};
+	 
+	page.onResourceReceived = function(response) {
+		system.stderr.writeLine('= onResourceReceived()' );
+		system.stderr.writeLine('  id: ' + response.id + ', stage: "' + response.stage + '", response: ' + JSON.stringify(response));
+	};
+	 
+	page.onLoadStarted = function() {
+		system.stderr.writeLine('= onLoadStarted()');
+		var currentUrl = page.evaluate(function() {
+			return window.location.href;
+		});
+		system.stderr.writeLine('  leaving url: ' + currentUrl);
+	};
+	 
+	page.onLoadFinished = function(status) {
+		system.stderr.writeLine('= onLoadFinished()');
+		system.stderr.writeLine('  status: ' + status);
+	};
+	 
+	page.onNavigationRequested = function(url, type, willNavigate, main) {
+		system.stderr.writeLine('= onNavigationRequested');
+		system.stderr.writeLine('  destination_url: ' + url);
+		system.stderr.writeLine('  type (cause): ' + type);
+		system.stderr.writeLine('  will navigate: ' + willNavigate);
+		system.stderr.writeLine('  from page\'s main frame: ' + main);
+	};
+	 
+	page.onResourceError = function(resourceError) {
+		system.stderr.writeLine('= onResourceError()');
+		system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
+		system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString );
+	};
+	 
+	page.onError = function(msg, trace) {
+		system.stderr.writeLine('= onError()');
+		var msgStack = ['  ERROR: ' + msg];
+		if (trace) {
+			msgStack.push('  TRACE:');
+			trace.forEach(function(t) {
+				msgStack.push('    -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+			});
+		}
+		system.stderr.writeLine(msgStack.join('\n'));
+	};
 	if(status !== 'success'){
 		console.log('Could not open page' + url);
 		phantom.exit(1);
 	}
-	*/
 	var ua = page.evaluate(function() {
 	  console.log("DOC" + document);
 	  //console.log(document.querySelectorAll('.pageheader')[0]);
