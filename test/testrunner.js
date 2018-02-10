995bb770b862df0a690fff78786ce84bf9c943e8
mazarag2 <mazarag2@asu.edu>
2018-02-10 15:58:23 -0700

    Update test for phantomjs

--- test/testrunner.js
+++ test/testrunner.js
@@ -2,155 +2,106 @@
 var page = require('webpage').create();
 var server = require('webserver').create();
 var system = require('system');
-var host, port;
+var fs = require('fs');
+var host;
 var port = "8080";
+var dotenv = require('../src/env');
+
+//LOCALLY USE ENV VAR THEN RESEARCH TRAVIS 
 
 
+console.log(process.env.API_KEY);
 
-
-// We need the Service 
-/*
-var service = server.listen(8081, {keepAlive:true}, function() {
-	
-});
-
-//if (service) {
-	//var page = new WebPage();
-	var url = "http://localhost:" + "8080/";
-	console.log("SENDING REQUEST TO:");
-	console.log(url);
-	page.open(url, function(status) {
-	  if (status !== 'success') {
-		console.log('Unable to access network');
-	  } else {
-		var ua = page.evaluate(function() {
-			console.log(document);
-		  console.log(document.getElementByClassName('pageheader').innerHTML);
-		  return document.getElementByClassName('pageheader').innerHTML;
-		});
-		if(!ua){
-			
-			console.log('Home page not found');
-			phantom.exit(1);
-		}
-	  }
-	  //phantom.exit();
-	});
-	page.open('http://localhost:8080/',function(status){
-	   console.log(status);
-	   page.render('home.png');
-	   phantom.exit();
-	});
-//}
-//else{
-	
-	console.log("Unable to create server");
-	phantom.exit(1);
-//}
-*/
 var url = 'http://127.0.0.1:8080/';
-page.open(url ,function(status) {
+
+
+page.open('http://localhost:8080/' ,function(status) {
 	console.log(status);
-	page.onResourceRequested = function (request) {
-		system.stderr.writeLine('= onResourceRequested()');
-		system.stderr.writeLine('  request: ' + JSON.stringify(request, undefined, 4));
-	};
-	 
-	page.onResourceReceived = function(response) {
-		system.stderr.writeLine('= onResourceReceived()' );
-		system.stderr.writeLine('  id: ' + response.id + ', stage: "' + response.stage + '", response: ' + JSON.stringify(response));
-	};
-	 
-	page.onLoadStarted = function() {
-		system.stderr.writeLine('= onLoadStarted()');
-		var currentUrl = page.evaluate(function() {
-			return window.location.href;
-		});
-		system.stderr.writeLine('  leaving url: ' + currentUrl);
-	};
-	 
-	page.onLoadFinished = function(status) {
-		system.stderr.writeLine('= onLoadFinished()');
-		system.stderr.writeLine('  status: ' + status);
-	};
-	 
-	page.onNavigationRequested = function(url, type, willNavigate, main) {
-		system.stderr.writeLine('= onNavigationRequested');
-		system.stderr.writeLine('  destination_url: ' + url);
-		system.stderr.writeLine('  type (cause): ' + type);
-		system.stderr.writeLine('  will navigate: ' + willNavigate);
-		system.stderr.writeLine('  from page\'s main frame: ' + main);
-	};
-	 
-	page.onResourceError = function(resourceError) {
-		system.stderr.writeLine('= onResourceError()');
-		system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
-		system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString );
-	};
-	 
-	page.onError = function(msg, trace) {
-		system.stderr.writeLine('= onError()');
-		var msgStack = ['  ERROR: ' + msg];
-		if (trace) {
-			msgStack.push('  TRACE:');
-			trace.forEach(function(t) {
-				msgStack.push('    -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
-			});
-		}
-		system.stderr.writeLine(msgStack.join('\n'));
-	};
+	
 	if(status != 'success'){
 		console.log('Could not open page' + url);
 		phantom.exit(1);
 	}
 	var ua = page.evaluate(function() {
-	  console.log("DOC" + document);
-	  //console.log(document.querySelectorAll('.pageheader')[0]);
-	  return document.getElementsByTagName('h1')[0].innerHTML;
+	    console.log("DOC" + document);
+	  
+	    return document.getElementsByTagName('h1')[0].innerHTML;
 	});
 	
 	if(!ua){
 		
 		console.log('Home page not found');
-		//phantom.exit(1);
+		phantom.exit(1);
 	}
 	else{
 		console.log("Success retreiving Title " + ua);
-		phantom.exit()
+		//phantom.exit()
 	}
 	
-	page.onResourceError = function(resourceError) {
-		 console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
-		  console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
-	};
-	page.onLoadFinished = function(status) {
-		
-		console.log(status);
-		system.stderr.writeLine('= onLoadFinished()');
-		system.stderr.writeLine('  status: ' + status);
-		if (status !== 'success') {
-		console.log('Unable to access network');
-		} 
-		else {
-			var ua = page.evaluate(function() {
-			  console.log("DOC" + document);
-			  //console.log(document.querySelectorAll('.pageheader')[0]);
-			  return document.getElementsByTagName('h1')[0].innerHTML;
-			});
-			
-			if(!ua){
-				
-				console.log('Home page not found');
-				phantom.exit(1);
-			}
-			else{
-				console.log("Success retreiving Title " + ua);
-				phantom.exit()
-			}
-		  }
-		
-	
-	};
 	
 });
+
+//setTimeout(function(){console.log(" On to Test Case 2 ")},3000);
+
+var indexPage = require('webpage').create();
+
+indexPage.open(url, function (status) {
+   
+    indexPage.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function () {
+   
+       
+        indexPage.onLoadFinished = function () {
+           
+            indexPage.render("after_submit.png");
+            console.log("Succesfull Login");
+			var ua = indexPage.evaluate(function() {
+				console.log("DOC" + document);
+				return document.getElementsByTagName('h1')[0].innerHTML;
+			});
+			console.log("Title after login " + ua);
+            phantom.exit();
+           
+        };
+       
+
+        indexPage.evaluate(function () {
+           
+            $('[name="email"]').val("mikez@email.com");
+			$('[name="password"]').val("test123");
+			$("button").click();
+        });
+       
+       
+        indexPage.render("before_submit.png");
+       
+       
+   
+    });
+});
+
+
+//console.log(" All testCases Passed");
+//phantom.exit();
+/*
+var createUser = 'http://127.0.0.1:8080/CreateUser';
+page.open(createUser,function(status){
+	
+	console.log(status);
+	
+	if(status != 'success'){
+		console.log('Could not open page ' + createUser);
+		phantom.exit(1);
+	}
+	var ua = page.evaluate(function() {
+	  console.log("DOC" + document);
+	  //console.log(document.querySelectorAll('.pageheader')[0]);
+	   var form =  document.getElementsByTagName('form')[0];
+	  
+	  
+	  
+	});
+	
+	
+}); 
+*/
 
