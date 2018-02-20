e12dd2500a3aeec00d294fa738dfff77d965bb32
mazarag2 <mazarag2@asu.edu>
2018-02-19 20:10:09 -0700

    Added auth test cases,refactored auth functions

---
+++ src/auth.js
@@ -0,0 +1,120 @@
+var auth = function() {
+	
+	var self = this;
+	var LoggedIn = false;
+	var firstName = "";
+	const NodeCache = require("node-cache");
+    const userCache = new NodeCache();
+	this.CreateUser = function(postData,firebase,userRef){
+		
+		var origEmail = postData['email'];
+		console.log(email);
+		var password = postData['password'];
+		var password2 = postData['password2'];
+		var firstName = postData['firstname'];
+		var lastName = postData['lastname'];
+		console.log(password);
+		if(password != password2){
+			
+			return "Passwords do not match please try again";
+			
+		}
+		else{
+		
+			var email = origEmail.toLowerCase();
+			email = email.replace(/\./g, ',');
+			
+			
+			var newQuery = new query();
+			
+				
+			// New User lets sign them up 
+
+			var promise = new Promise(function(resolve){
+				firebase.auth().createUserWithEmailAndPassword(origEmail, password).catch(function(error) {
+				
+					var errorMessage = error.message;
+					console.log(errorMessage)
+					resolve(errorMessage);
+
+				})
+		
+			});
+			
+			return promise.then(function(resolve){
+				
+			
+				
+				if(resolve){
+					
+					return resolve;
+				}
+				else{
+					//succesfully created
+					firebase.auth().onAuthStateChanged(function(user) {
+						  if (user) {
+							console.log(user);
+							//var ref = database.ref('Users');
+							
+							var id = userRef.child(email);
+							var newRef = id.push();
+							
+							newRef.set({
+								firstname: firstName ,
+								lastname : lastName 
+							})
+							
+						  } else {
+							// User is signed out.
+						  }
+					});
+					return "Account Created Succesfully!";
+					
+				}
+				
+				
+			});
+			
+		
+		}
+	}
+	this.signInUser = function(email,password,firebase){
+	
+	
+		var promise = new Promise(function (resolve,reject){
+			
+			
+			firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+				var errorMsg = "Email and Password Incorrect please try again";
+				reject(error.message);
+				console.log(error.message);
+				
+				//res.render('login',{errorMsg : errorMsg});
+			});
+			
+			firebase.auth().onAuthStateChanged(function(user) {
+				  if (user) {return resolve(true);}
+			});
+			console.log('we out');
+			
+		});
+		
+		return promise.then(function(resolve,reject){
+			
+			console.log(resolve + '--' + reject);
+			if(reject){
+				
+				return reject;
+				
+			}
+			else{
+				return resolve;
+			}
+			
+			
+			
+			
+		})
+	}
+};
+module.exports = auth;
--- test/mochaTest.js
+++ test/mochaTest.js
@@ -1,7 +1,9 @@
 var assert = require('assert');
 const query = require("../src/query");
+const auth = require("../src/auth");
 var firebase = require("firebase");
 const dotev = require('dotenv').config();
+var expect = require('chai').expect;
 var config = {
     apiKey : process.env.API_KEY,
     authDomain: "learnlive-f6376.appspot.com",
@@ -19,11 +21,13 @@ var courseRef = firebase.database().ref("Courses");
 var userRef = firebase.database().ref("Users");
 var enrollmentRef = firebase.database().ref("Enrollment");
 var instructorRef = firebase.database().ref("Instructors");
+var existingEmail = process.env.TEST_EMAIL;
+var password1 = process.env.TEST_PASSWORD;
 
 describe('Create', function() {
-  describe('#CheckifUserExists()', function() {
+  describe('#CheckUserFunctions()', function() {
 	    
-    it('should return an error msg for pre-existing users', function() {
+    it('should return an error msg for Creating an Account with Existing email', function() {
 		
 		const errorMsg = "The email address is already in use by another account.";
 		var newQuery = new query();
@@ -39,8 +43,6 @@ describe('Create', function() {
 			lastname : "Z"
 			
 		}
-
-		
 		const resolvingPromise = new Promise(function(resolve){
 			
 			resolve(newQuery.CreateUser(postData,firebase,userRef));
@@ -53,6 +55,47 @@ describe('Create', function() {
 		})
 		
     });
+	
+	it('should return true logging in Existing User',function(done){
+		
+		const result = true;
+		
+		var newAuth = new auth();
+		
+		const resolvingPromise = new Promise(function(resolve){
+			
+			resolve(newAuth.signInUser(existingEmail,password1,firebase));
+			done();
+			
+		});
+		return resolvingPromise.then(function(resolve){
+			
+			console.log(resolve);
+			assert.equal(result,resolve);
+			
+		})
+	});
+	
+	it('should fail logging in non-existant user',function(){
+		
+		const result = "The password is invalid or the user does not have a password.";
+		
+		var newAuth = new auth();
+		
+		var password2 = process.env.TEST_PASSWORD2;
+		
+		const resolvingPromise = new Promise(function(resolve,reject){
+			
+			resolve(newAuth.signInUser(existingEmail,password2,firebase));
+			done();
+			
+		});
+		return resolvingPromise.catch(function(e) {
+			expect(e).to.equal(result);
+		})
+		
+	});
+	
 	
   });
 });
